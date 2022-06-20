3aeeacef8155aa58936875e7a830297af59de785
Daniel Prieto Gaviria <prietogaviriadaniel@gmail.com>
2022-06-20 14:56:01 +0200

    añadiendo estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,19 +9,19 @@ function Home() {
   return (
     <main className="home-container">
         <header>
-          <Link className="home" to='/'>DPG - Games</Link>
+          <Link className="home" to='/'>FreeGames</Link>
         </header>
 
         <section className="inicio">
           <aside>
             <h1>FREE GAMES</h1>
-            <p>FIND NEW GAMES</p>
+            <p>Game list for browser and pc</p>
             <div className="links">
               <Link className="search" to="/search">SEARCH BY TITLE</Link>
               <Link className="filters" to='/games'>SEARCH BY FILTERS</Link>
             </div>
           </aside>
-          <img src="" alt="" />
+          <img className="img-inicio" src="../../god-of-war.webp" alt="god-of-war" />
         </section>
 
         <section className="links-options">
